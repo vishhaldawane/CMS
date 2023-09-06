bee0cea48c4294589c502768d3f5f1954f74a1ea
vishhaldawane <vishalviniing@gmail.com>
2023-09-06 11:14:06 +0530

    access specifiers

--- PackageProject/src/jungle/house/FarmHouse.java
+++ PackageProject/src/jungle/house/FarmHouse.java
@@ -1,4 +1,5 @@
 package jungle.house;
+import jungle.cave.Tiger;
 import jungle.river.Frog;
 import jungle.tree.Parrot;
 
@@ -15,6 +16,42 @@ public class FarmHouse {
 		Frog frog = new Frog();
 		frog.jump();
 		frog.swim();
-		frog.hiberate();
+		//frog.hiberate();
+	}
+}
+
+class BengalTiger extends Tiger
+{
+	void jump() {
+		
+		System.out.println("defaultA   "+super.defaultA);
+		System.out.println("privateB   "+super.privateB);
+		System.out.println("protectedC "+super.protectedC);
+		System.out.println("publicD    "+super.publicD);
+		
+		System.out.println("White tiger is jumping..");;
+	}
+}
+class Fox
+{
+	void show() {
+		Tiger tiger = new Tiger();
+		System.out.println("defaultA   "+tiger.defaultA);
+		System.out.println("privateB   "+tiger.privateB);
+		System.out.println("protectedC "+tiger.protectedC);
+		System.out.println("publicD    "+tiger.publicD);
+		
+	}
+}
+
+class Lion
+{
+	void show() {
+		Tiger tiger = new Tiger();
+		System.out.println("defaultA   "+tiger.defaultA);
+		System.out.println("privateB   "+tiger.privateB);
+		System.out.println("protectedC "+tiger.protectedC);
+		System.out.println("publicD    "+tiger.publicD);
+		
 	}
 }
